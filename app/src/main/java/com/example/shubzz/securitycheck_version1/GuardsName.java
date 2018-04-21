624302cf62c706ff7ffa09d25b236236731cd4bb
shubham15098 <shubham15098@iiitd.ac.in>
2018-04-22 02:22:30 +0530

    ishii ki activity :3

---
+++ app/src/main/java/com/example/shubzz/securitycheck_version1/GuardsName.java
@@ -0,0 +1,13 @@
+package com.example.shubzz.securitycheck_version1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuardsName extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guards_name);
+    }
+}
