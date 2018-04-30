247c23282e3eff654988de86d690b8ebdbea9dc8
shubham15098 <shubham15098@iiitd.ac.in>
2018-04-30 15:45:37 +0530

    sexy ui <3

--- app/src/main/java/com/example/shubzz/securitycheck_version1/Fragment_Home.java
+++ app/src/main/java/com/example/shubzz/securitycheck_version1/Fragment_Home.java
@@ -1,12 +1,14 @@
 package com.example.shubzz.securitycheck_version1;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
 public class Fragment_Home extends Fragment {
@@ -29,7 +31,63 @@ public class Fragment_Home extends Fragment {
                              Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment__home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_fragment__home, container, false);
+
+//        ImageButton img = (ImageButton) rootView.findViewById(R.id.areasIcon);
+//        img.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v)
+//            {
+//                Intent i = new Intent(getActivity(),AreasActivity.class);
+//                startActivity(i);
+//            }
+//        });
+
+        ImageButton areas=(ImageButton)rootView.findViewById(R.id.areasIcon);
+        ImageButton gaurds=(ImageButton)rootView.findViewById(R.id.guardsIcon);
+        ImageButton supervisor=(ImageButton)rootView.findViewById(R.id.supervisorIcon);
+        ImageButton star=(ImageButton)rootView.findViewById(R.id.callIcon);
+        ImageButton graphs=(ImageButton)rootView.findViewById(R.id.supervisorIcon2);
+        ImageButton weeklyreport=(ImageButton)rootView.findViewById(R.id.weeklyIcon2);
+
+        areas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),AreasActivity.class);
+                startActivity(i);
+            }});
+        gaurds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),GuardsName.class);
+                startActivity(i);
+            }});
+        supervisor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),SupervisorWalk.class);
+                startActivity(i);
+            }});
+        star.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),StarOfTheWeek.class);
+                startActivity(i);
+            }});
+        graphs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),Graphs.class);
+                startActivity(i);
+            }});
+        weeklyreport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(),WeeklyReport.class);
+                startActivity(i);
+            }});
+
+        return rootView;
     }
 
 
